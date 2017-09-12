0d76f1e7575ccb1be37b651778db2f54caff176a
Hugh Garner <hugh.garner@bristol.ac.uk>
2017-09-12 10:39:34 +0100

    - added call from command line processing - dumps output as json to file

--- processing_time.py
+++ processing_time.py
@@ -7,8 +7,9 @@ import configparser
 import argparse
 from copy import deepcopy
 from pprint import pprint
-from datetime import timedelta
+from datetime import timedelta, datetime
 from numpy import average, percentile
+import json
 
 def processCsv(csv_filename):
   try:
@@ -61,11 +62,63 @@ def summaryStats(samples):
 
   return {
       'proc_times': {
+        'units': 'seconds',
         'mean': proc_avg,
         '95_cent': proc_95_cent
       }
     }
 
+def loadConfig(config_filename = '', config = None):
+  root_dir = os.getcwd()
+  if config is None:
+    config = configparser.ConfigParser()
+  config.read(os.path.join(root_dir, config_filename))
+  return config
+
+if __name__ == '__main__':
+  global config 
+  path_to_script = os.path.abspath(sys.argv[0])
+  root_dir = os.path.dirname(os.path.dirname(path_to_script))
+
+  parser = argparse.ArgumentParser(description='Generate summary stats from sample processing times etc')
+  parser.add_argument('--input', dest='input_filename', action='store', help='input csv filename')
+  parser.add_argument('--outdir', dest='output_dir', action='store', help='output directory')
+  parser.add_argument('--config', dest='config_filename', action='store', help='optional config .ini file')
+  global args
+  args = parser.parse_args()
+  try:
+    config = load_config(args.config_filename, config)
+  except:
+    pass
+
+  if args.input_filename is not None:
+    input_filepath = os.path.abspath(args.input_filename)
+    input_filename = os.path.split(os.path.abspath(input_filepath))[1]
+    samples = processCsv(input_filepath)
+    summary = summaryStats(samples)
+    summary['input_filename'] = input_filename
+
+    # dump the data to a file
+
+    now = datetime.now()
+    datestamp = '{now.day:0>2}{now.month:0>2}{now.year}'.format(now = now) 
+
+    output_filepath = re.sub('\.csv$', '_summarystats.{date}.json'.format(date = datestamp), input_filepath)
+
+    if args.output_dir is not None:
+      output_dir = args.output_dir
+    else:
+      output_dir = os.path.split(input_filepath)[0]
+
+    with open(os.path.join(output_dir, output_filepath), 'w') as output:
+      json.dump(summary, output, indent=2)
+
+    print('summary_stats: successfully output to {output_filepath}'.format(output_filepath = output_filepath))
+    exit(0)
+
+  else:
+    print('--input argument missing: please specify a csv file to process')
+    exit(1)
 
 
 
