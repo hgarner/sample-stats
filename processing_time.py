2893b807603005f9804e73ddaa6305eb2ca4de79
Hugh Garner <hugh.garner@bristol.ac.uk>
2019-03-21 16:22:41 +0000

    - updated to process target times for studies and subsets

--- processing_time.py
+++ processing_time.py
@@ -30,11 +30,17 @@ def processCsv(csv_filename):
               samples[key] = [val]
 
     return samples
-  except:
+  except Exception as e:
+    print(e)
     print('processCsv: Error processing csv file')
     exit(1)
   
-def summaryStats(samples):
+# generate summary stats from dict of samples
+# @param dict samples dict of samples data with keys inc 'sample_id',
+# 'time_taken', etc. each key is a list of sample data
+# @param int target_time in minutes for sample processing
+# @return dict processing time stats for this subset of samples
+def summaryStats(samples, target_time = 90):
   ##
   # check if any samples to ignore
   #
@@ -62,11 +68,25 @@ def summaryStats(samples):
     exit(1)
   
   # convert to timedeltas
+  target_time_seconds = timedelta(minutes = target_time).total_seconds()
+  # list for processing times
   proc_times = []
+  # counter for samples above target_time cutoff
+  above_target = 0
+  # counter for total samples
+  sample_count = 0
   for index, str_time in enumerate(raw_proc_times):
     if index not in ignore_indexes:
+      # sample should be included as not in ignore_indexes
+      # increment sample_count
+      sample_count += 1
+      # work out the processing time
       time = [int(t) for t in str_time.split(':')]
       proc_times.append((timedelta(hours = time[0], minutes = time[1], seconds = time[2])).total_seconds())
+      # increment above_target if processing time > target_time
+
+      if proc_times[-1] > target_time_seconds:
+        above_target += 1
 
   proc_times.sort()
 
@@ -82,6 +102,9 @@ def summaryStats(samples):
 
   return {
       'proc_times': {
+        'count': sample_count,
+        'above_target': above_target,
+        'target_time': target_time,
         'units': 'seconds',
         'mean': proc_avg,
         '95_cent': proc_95_cent,
@@ -123,15 +146,6 @@ def splitByStudy(samples):
   # check if some sam_codes should be separated from bulk of study data
   #
 
-  # if there is a config setting for separating a group of sam_codes
-  # from the rest of the study data, make sure we do this
-  # if not, use the key 'all_relevant'
-
-  subset_structure = {
-    'sam_codes': [],
-    'samples': {field_name: [] for field_name in samples.keys()},
-  }
-
   # set up lookup for sam_code to point to relevant samples array 
   sam_codes_lookup = {}
 
@@ -145,15 +159,45 @@ def splitByStudy(samples):
 
     this_study_sam_codes = [study_code for study_code in sam_codes_bystudy if study_code[1] == short_code]  
 
+    # see if config has a default target_time for this study
+    target_time = 90
+    try:
+      study_config = json.loads(config['studies'][short_code])
+      target_time = int(study_config['target_time'])
+    except KeyError:
+      pass
+
+    # if there is a config setting for separating a group of sam_codes
+    # from the rest of the study data, make sure we do this
+    # if not, use the key 'all_relevant'
+
+    subset_structure = {
+      'sam_codes': [],
+      'target_time': target_time,
+      'samples': {field_name: [] for field_name in samples.keys()},
+    }
+
     # see if we can get any subset structure specified in the config
     try:
       # add any subsetted sam_code/short_code combinations to sam_codes_lookup
+      # we expect there to be an object with keys for each subset name
+      # each subset name key contains an object with keys:
+      #  - 'sam_codes' : list of sam_codes in this subset
+      #  - 'target_time' (optional) : target processing time for subset
       separate_sam_codes = json.loads(config['study_subsets'][short_code])
       
-      for subset_name, subset_codes in separate_sam_codes.items():
+      for subset_name, subset_data in separate_sam_codes.items():
+
         samples_bystudy[short_code][subset_name] = deepcopy(subset_structure)
 
-        for subset_code in subset_codes:
+        # try to add the target_time for this subset if present
+        try:
+          samples_bystudy[short_code][subset_name]['target_time'] = int(subset_data['target_time'])
+        except KeyError:
+          pass
+
+        # add the sam_codes for this subset
+        for subset_code in subset_data['sam_codes']:
           samples_bystudy[short_code][subset_name]['sam_codes'].append(subset_code)
           sam_codes_lookup[(subset_code, short_code)] = samples_bystudy[short_code][subset_name]['samples']
 
@@ -169,6 +213,7 @@ def splitByStudy(samples):
       if sam_code_study not in sam_codes_lookup.keys():
         samples_bystudy[short_code]['all_relevant']['sam_codes'].append(sam_code_study[0])
         sam_codes_lookup[sam_code_study] = samples_bystudy[short_code]['all_relevant']['samples']
+
 
   ##
   # split the samples 
@@ -192,7 +237,16 @@ def load_config(config_filename = '', config = None):
   config.read(os.path.join(root_dir, config_filename))
   return config
 
+# run from cmd line
+# get the data file containing processing time for all core samples 
+# (from the 'input' arg)
+# load a config describing how to show the output (split by sam_code etc)
+# process the data by study and config settings to get avg time etc
+# save to outdir arg
+# dumps a json file of summary stats
+# outputs html that produces graphs of summary stats
 if __name__ == '__main__':
+  # get the root path we're running from
   path_to_script = os.path.abspath(sys.argv[0])
   root_dir = os.path.dirname(os.path.dirname(path_to_script))
 
@@ -225,7 +279,7 @@ if __name__ == '__main__':
     for short_code, sampleset in samples.items():
       for subset_name, subset_data in sampleset.items():
         if len(subset_data['samples']['sam_code']) > 0:
-          summary['{short_code}_{subset_name}'.format(short_code=short_code, subset_name=subset_name)] = summaryStats(subset_data['samples'])
+          summary['{short_code}_{subset_name}'.format(short_code=short_code, subset_name=subset_name)] = summaryStats(subset_data['samples'], target_time = subset_data['target_time'])
         else:
           summary['{short_code}_{subset_name}'.format(short_code=short_code, subset_name=subset_name)] = 0
 
